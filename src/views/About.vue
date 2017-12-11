853e3cbd2b8a91303276116c9f940e45bba9e98f
gitcjh <1418503647@.qq.com>
2017-12-11 15:29:12 +0800

    完成模板

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="page-about">
+        <ui-header></ui-header>
+        <main class="page-body">
+            <div class="container">
+                这是关于页面
+            </div>
+        </main>
+        <ui-footer></ui-footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>

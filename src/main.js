6deef3194c91b60410db7c350c1de6da8c48f593
gitcjh <1418503647@.qq.com>
2017-12-27 11:08:11 +0800

    添加 http 功能

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import http from '@/util/http'
+import qs from 'qs'
+import storage from '@/util/storage'
 import './scss/main.scss'
 import ui from './components/index'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = http
+Vue.prototype.$qs = qs
+Vue.prototype.$storage = storage
 
 Vue.use(ui)
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,8 @@
         <ui-header></ui-header>
         <main class="page-body">
             <div class="container">
-                这是关于页面
+                <div>这是关于页面</div>
+                <div>版本：{{ version }}</div>
             </div>
         </main>
         <ui-footer></ui-footer>
@@ -14,7 +15,34 @@
     export default {
         data () {
             return {
+                version: ''
             }
+        },
+        mounted() {
+            this.$http.get('/version').then(
+                response => {
+                    let data = response.data
+                    if (data.code === 200) {
+                        console.log(data)
+                        this.version = data.data
+                    }
+                },
+                response => {
+                    console.log(response)
+                })
+//            this.$http.post('/example', this.$qs.stringify({
+//                param1: 'value1',
+//                param2: 'value2'
+//            })).then(
+//                response => {
+//                    let data = response.data
+//                    if (data.code === 200) {
+//                        console.log(data.data)
+//                    }
+//                },
+//                response => {
+//                    console.log(response)
+//                })
         }
     }
 </script>

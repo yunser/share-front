c197d91ad4387d6995de3d8a93b6dbfe87114787
gitcjh <1418503647@.qq.com>
2018-01-05 10:13:37 +0800

    不知道能不能运行，先备份再说

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,6 +1,12 @@
 <template>
     <header class="page-header">
+        <ui-appbar title="标题">
+            <ui-icon-button icon="menu" slot="left"/>
+            <ui-flat-button label="expand_more" slot="right"/>
+            <ui-icon-button icon="expand_more" slot="right"/>
+        </ui-appbar>
         <div class="container">
+            <!--<ui-raised-button label="Default" class="demo-raised-button"/>-->
             <div>这是页面头部</div>
         </div>
     </header>
